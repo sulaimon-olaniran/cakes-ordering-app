ba3ae888edae9ddd4709fda8b9326166ab3823c2
Olaniran Sulaimon <suzzwayne@gmail.com>
2020-04-21 14:39:23 +0100

    extra tests

--- src/TestFormPage.js
+++ src/TestFormPage.js
@@ -3,13 +3,20 @@ import React from 'react'
 const TestFormPage = () => {
 
     return (
-        <form name="userName" method="Post" data-netlify="true" data-netlify-recaptcha="true">
-            <input type="text" name="firstName" placeholder="first name" />
-            <input type ="text" name="otherName" placeholder="other name" />
-            <input type="text" name="lastName" placeholder="last name" />
-             
-             <div data-netlify-recaptcha="true" ></div>
-            <input type="submit" value="send names" />
+        <form name="contact" method="post">
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>Your Name: <input type="text" name="name"/></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email"/></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
         </form>
     )
 }
