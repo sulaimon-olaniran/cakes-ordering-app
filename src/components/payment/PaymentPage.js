303ea848f1c5df643ecf0ebc2249397c4ab61749
Olaniran Sulaimon <suzzwayne@gmail.com>
2020-04-29 03:37:24 +0100

    integrating react-paystack

--- src/components/payment/PaymentPage.js
+++ src/components/payment/PaymentPage.js
@@ -1,14 +1,61 @@
 import React from 'react'
 import Paystack from './Paystack'
+import './Payment.css'
+import { withFormik, Form, Field } from 'formik'
+import * as yup from 'yup'
 
 
-const PaymentPage = () => {
+const PaymentPage = ({values, errors, touched}) => {
+    const {buyerName, buyerEmail, buyerNumber, orderCode, amount} = values
     return (
-        <div>
-            <h1>Make Payment with Paystack</h1>
-        <Paystack />
+        <div className="payment-con">
+            <div className="instructions">
+                <h3>Please Note</h3>
+                <p>Enter your valid email to receive payment receipt (optional)</p>
+                <p>Enter the 75% agreed amount to pay</p>
+                <p>Full Name should be same name of buyer that placed order for cake</p>
+                <p>Please enter the correct order-code</p>
+            </div>
+            <div className="payment-form-con">
+                 <Form className="payment-form">
+                     <Field type="text"  name="buyerName" placeholder="Full Name" className="payment-field"/>
+                     {touched.buyerName && errors.buyerName && <small>{errors.buyerName}</small> }
+                
+                     <Field type="email" name="buyerEmail" placeholder="Email" className="payment-field"/>
+                     <Field type="number" name="buyerNumber" placeholder="Phone Number.." className="payment-field"/>
+                     <Field type="text" name="orderCode" placeholder="Order Code.." className="payment-field"/>
+                     {touched.orderCode && errors.orderCode && <small>{errors.orderCode}</small> }
+                     <p>&#8358; <Field type="number" name="amount" placeholder="Amount to pay" className="payment-field-money"/></p>
+                     {touched.amount && errors.amount && <small>{errors.amount}</small> }
+                 </Form>
+            </div>
+            <Paystack buyer={buyerName} number={buyerNumber} email={buyerEmail} code={orderCode} amount={amount} />
         </div>
     )
 }
 
-export default PaymentPage
+
+const FormikPaymentPage = withFormik({
+    mapPropsToValues() {
+        return {
+            buyerNumber: "",
+            buyerName : "",
+            buyerEmail : "",
+            amount : 0 || "",
+            orderCode :""
+        }
+    },
+    validationSchema:yup.object().shape({
+        buyerName: yup.string().required("Buyer Full Name is Required"),
+        orderCode: yup.string().required("Order code is required"),
+        amout: yup.string().required("Payment Amount is Required")
+
+    }),
+
+    handleSubmit(values) {
+        console.log(values)
+    }
+
+})(PaymentPage)
+
+export default FormikPaymentPage
--- src/components/payment/Paystack.js
+++ src/components/payment/Paystack.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import './Payment.css'
 import PaystackButton from 'react-paystack'
 
-const key =  "pk_test_########################################"
-const Paystack =() => {
-
+const key = "pk_test_41ee8583da081526072608e37e6daff1dd0b9cec"
+const Paystack = ({buyer, number, email, code, amount}) => {
+    const disabled = buyer === "" || code === "" ? true : false
+    //const convertedAmout = amount * 100;
     const callback = (response) => {
         console.log(response); // card charged successfully, get reference here
     }
@@ -12,36 +14,79 @@ const Paystack =() => {
         console.log("Payment closed");
     }
 
-   const getReference = () => {
+    const getReference = () => {
         //you can put any unique reference implementation code here
         let text = "";
-    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-.=";
+        let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-.=";
 
-        for( let i=0; i < 15; i++ )
+        for (let i = 0; i < 15; i++)
             text += possible.charAt(Math.floor(Math.random() * possible.length));
 
         return text;
     }
-
+    
     return (
+
         <div>
-        
-          <PaystackButton
-            text="Make Payment"
-            className="payButton"
-            callback={callback}
-            close={close}
-            disabled={true} 
-            embed={true} 
-            reference={getReference()}
-            email={"suzzwayne@gmail.com"}
-            amount={5000}
-            paystackkey={key}
-            tag="button"
-          />
-        
-      </div>
+            <p>
+                <PaystackButton
+                    metadata={{
+                       "custom_fields": [
+                            {
+                                "display_name" : "Mobile Number",
+                                "variable_name" : "mobile_number",
+                                "value" : {number} 
+                            },
+                        
+                            {
+                                "display_name" : "Buyer Name",
+                                "variable_name" : "buyer_name",
+                                "value" : {buyer}
+                            }, 
+                            {
+                                "display_name" : "Order Code",
+                                "variable_name" : "order_code",
+                                "value" : {code} 
+                            },
+                        ]
+                    }}
+                    text="Pay with Paystack"
+                    className="payButton"
+                    callback={callback}
+                    close={close}
+                    disabled={disabled}
+                    embed={false}
+                    reference={getReference()}
+                    email={email}
+                    amount={amount * 100}
+                    paystackkey={key}
+                    tag="button"
+                />
+            </p>
+        </div>
     )
 }
 
 export default Paystack
+
+
+
+
+/*
+ metadata= {{
+                    custom_fields: [
+                        {
+                            display_name: "Mobile Number",
+                            variable_name: "mobile_number",
+                            value: "+2348012345678" //customer's mobile number
+                        }
+                    ],
+                    custom_fields: [
+                        {
+                            display_name: "First Name",
+                            variable_name: "first_name",
+                            value: "+2348012345678"
+                        }
+                    ]
+                }}
+*/
